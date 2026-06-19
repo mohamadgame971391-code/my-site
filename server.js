d520c1bfc5a6c95cd73acb1b04cca33de0f61db9
mohamadgame971391 <mohamadgame971391@gmail.com>
2026-06-19 20:26:57 +0330

    fix express static

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const app = express();
 
 app.use(express.json());
-.app.use(express.static("public"))
+;app.use(express.static("public"))
 
 app.post("/save",(req,res)=>{
   console.log("DATA:", req.body);
